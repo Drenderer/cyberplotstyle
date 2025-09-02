e447ff3eb65f9347db453c13f17432d55736acda
Fabian Roth <roth@cps.tu-darmstadt.de>
2025-09-02 14:36:42 +0200

    Reworked package layout and added colormaps.

--- src/cyberplotstyle/__init__.py
+++ src/cyberplotstyle/__init__.py
@@ -1,15 +1,26 @@
 
 from pathlib import Path
+from warnings import warn
 
+import matplotlib as mpl
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 
-from .colors import cps_colors, tud_colors
-from .colors import scale_hls as scale_hls
+from .colormaps import colormaps
+from .colors import cps_colors as cps_colors
+from .colors import tud_colors as tud_colors
+from .tools import scale_hls as scale_hls
 
-# Set the CPS sytle
+# Activate CPS sytle
 plt.style.use(Path(__file__).parent / "cps.mplstyle")
 
-# Updated the named colors
+# Register named colors
 mcolors.get_named_colors_mapping().update(cps_colors)
 mcolors.get_named_colors_mapping().update(tud_colors)
+
+# Register colormaps
+for name, cmap in colormaps.items():
+    try:
+        mpl.colormaps.register(cmap, name=name, force=False)
+    except ValueError as e:
+        warn(f"A colormap with name '{name}' is already registered: {e}")
